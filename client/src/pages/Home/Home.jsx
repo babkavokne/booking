55781ed095b7fd7801bbae7f16a8c3bd5530e3ac
froggush <babkavokne@gmail.com>
2022-06-15 21:58:58 +0300

    начал HomePage, закончил правки createOffer

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Navbar/Navbar';
 import Navtop from '../../components/Navtop/Navtop';
+import OfferSearchBar from './components/OfferSearchBar/OfferSearchBar';
 import cl from './Home.module.sass'
 
 const Home = () => {
@@ -9,6 +10,13 @@ const Home = () => {
     <>
       <Navtop />
       <Navbar />
+      <div className="wrapper">
+        <div className="container">
+          <div className={cl.home}>
+            <OfferSearchBar className={cl.searchbar} />
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
